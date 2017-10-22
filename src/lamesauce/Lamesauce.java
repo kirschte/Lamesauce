aa28beff18775672b86816a46c3b1064848424f0
Moritz Kirschte <moritz.kirschte@gmail.com>
2017-10-22 14:57:53 +0200

    new telgramapi

--- src/lamesauce/Lamesauce.java
+++ src/lamesauce/Lamesauce.java
@@ -9,8 +9,6 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Locale;
-import java.util.TimeZone;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.telegram.telegrambots.*;
